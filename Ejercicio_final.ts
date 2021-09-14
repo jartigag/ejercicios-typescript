ce312bac288c04688f2906071d9807357c05b933
jartigag <jartigag@pm.me>
2021-09-14 18:52:57 +0200

    ejercicio final: propiedades mejor siempre privadas por defecto

--- Ejercicio_final.ts
+++ Ejercicio_final.ts
@@ -1,27 +1,17 @@
 /*
-    Hemos copiado y pegado un código que teníamos en Javascript y queremos 
+    Hemos copiado y pegado un código que teníamos en Javascript y queremos
     migrarlo a TypeScript. ¿Nos ayudas a conseguirlo? Aprovecha para probar
     todo lo que quieras sobre este lenguaje de programación
 */
 
 class Pokemon {
-  constructor(nombre, numero, ataques) {
-    this.nombre = nombre;
-    this.numero = numero;
-    this.ataques = ataques;
-  }
+  constructor(private nombre: string, private numero: number, private ataques: Array<string>) {}
 
-  ataque(numero) {
+  ataque(numero: number) {
     if (this.ataques.length >= numero && 0 < numero) {
-      console.log(
-        this.nombre + " utiliza el ataque " + this.ataques[numero - 1]
-      );
+      console.log(`${this.nombre} utiliza el ataque ${this.ataques[numero - 1]}`);
     } else {
-      console.log(
-        "Inserte un valor de ataque valido (valor entre 1 y " +
-          this.ataques.length +
-          ")"
-      );
+      console.log(`Inserte un valor de ataque valido (valor entre 1 y ${this.ataques.length})`);
     }
   }
 }
